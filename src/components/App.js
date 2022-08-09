9e3cc26065fdc0eab8188c49c9d3ee0414e11511
guilhermesmatiola <guimatiola@gmail.com>
2022-08-09 18:33:56 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import {React,useState} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import TrendingHashtag from "./TrendingHashtag";
+
+export default function App(){
+    const [user, setUser] = useState({});
+    return(
+        <UserContext.Provider value={{ user, setUser }}>
+        <BrowserRouter>
+            <Routes>
+                
+                {/* <Route path="/hashtag/:hashtag" element={<TrendingHashtag/>} />      */}
+                <Route path="/hashtag" element={<TrendingHashtag/>} />     
+            </Routes>
+        </BrowserRouter>
+        </UserContext.Provider>
+    );
+}
---
+++ src/components/TrendingHashtag.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from 'styled-components';
+import axios from 'axios';
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useEffect, useContext, useState } from 'react';
+import UserContext from "../context/UserContext";
+//import { ThreeDots } from "react-loader-spinner";
+
+export default function TrendingHashtag(){
+
+    const navigate = useNavigate();
+    const { user } = useContext(UserContext);
+    const { name, email, token } = user;
+
+    
+
+
+}
