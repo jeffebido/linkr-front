baf644445ef7bb0478bd23d5d4a7ca507464e060
João Marcelo Simão de Castro <jm.castro@usp.br>
2022-08-15 13:52:10 -0300

    signin and singup pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,39 @@
-import { React } from "react";
+import { React, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SignIn from "./pages/signInPage";
-import SignUp from "./pages/signUpPage";
-import TrendingHashtag from "./TrendingHashtag";
-import ResetStyle from "../assets/resetStyle";
+import SignIn from "./pages/SignIn/signInPage";
+import SignUp from "./pages/SignUp/signUpPage";
 
-// import UserContext from "../context/UserContext";
-import { AuthProvider } from "../context/AuthContext";
+import UserContext from "../context/UserContext";
 
 export default function App() {
-  // const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
+  const [image, setImage] = useState(null);
+  const [token, setToken] = useState(null);
+  const [updateListPosts, setUpdateListPosts] = useState(0);
+
   return (
-    // <UserContext.Provider value={{ user, setUser }}>
-    <AuthProvider>
-      <ResetStyle />
-      <BrowserRouter>
+    <BrowserRouter>
+      <UserContext.Provider
+        value={{
+          user,
+          setUser,
+          image,
+          setImage,
+          token,
+          setToken,
+          setUpdateListPosts,
+          updateListPosts,
+        }}
+      >
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          {/* <Route path="/hashtag/:hashtag" element={<TrendingHashtag/>} /> */}
-          <Route path="/hashtag" element={<TrendingHashtag />} />
+          {/* <Route path="/timeline" element={<Timeline />} />
+          <Route path="/hashtag/:hashtag" element={<Trend />} />
+          <Route path="/user/:id" element={<UserPage />} /> */}
         </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-    // </UserContext.Provider>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
